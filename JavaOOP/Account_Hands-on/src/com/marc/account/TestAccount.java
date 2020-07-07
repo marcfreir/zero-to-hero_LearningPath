9a847bdac38b86659984e75ecc213f797a7bc596
Marc Freir <marcfreir@outlook.com>
2020-07-06 23:25:02 -0200

    Added random data and method print

--- JavaOOP/Account_Hands-on/src/com/marc/account/TestAccount.java
+++ JavaOOP/Account_Hands-on/src/com/marc/account/TestAccount.java
@@ -4,6 +4,7 @@ public class TestAccount
 {
     public static void main(String[] args)
     {
+        //Some random data for Timmy
         Account account = new Account("Timmy", 123, 500.42);
         account.setName("Timmy");
         account.print();
